feaa59370a79fa451d91417e7f02cd5504933482
Jah109Fc <[jah109fc@gmail.com]>
2023-09-03 16:42:34 -0400

    Additional Exercise 1 Commit

---
+++ morepyth.py
@@ -0,0 +1,8 @@
+# Get input from the user
+input_string = input("Enter a string: ")
+
+# Reverse the input string
+reversed_string = input_string[::-1]
+
+# Print the reversed string
+print("Reversed string:", reversed_string)
